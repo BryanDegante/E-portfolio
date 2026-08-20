31ea649211193ea61b3ec005fbafcc32eb47d967
Bryan <bdegante0906@gmail.com>
2026-08-20 17:54:01 -0500

    tiers are expanding and collapsing

--- src/components/Services/Tiers.jsx
+++ src/components/Services/Tiers.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TierCard from '../UI/TierCard';
 import { TierData } from '../../data/TierData';
 
 const Tiers = () => {
+	const [ activeTier, setActiveTier ] = useState(null);
+
+	
+	const tierSelect = (title) => {
+		if(title === activeTier) return setActiveTier(null)
+		setActiveTier(title)
+	}
+	
+	
+			
 	return (
 		<section id="Tiers">
 			<div className="container">
-				<h2 className="text__color--normal">TIERS</h2>
-				<div className="tier__text text__color--normal">
-					<h3>Choose the right website for your needs.</h3>
-					<p>
-						Whether you need a simple online presence or a fully
-						customized experience, <br /> there's an option to fit
-						your project
-					</p>
+				<div className="Tiers__header">
+					<h2 className="text__color--normal">TIERS</h2>
+					<div className="tier__text text__color--normal">
+						<h3>Choose the right website for your needs.</h3>
+						<p>
+							Whether you need a simple online presence or a fully
+							customized experience, <br /> there's an option to
+							fit your project
+						</p>
+					</div>
 				</div>
 				<div className="tier__cards">
 					{TierData.map((tier, index) => (
@@ -24,8 +36,11 @@ const Tiers = () => {
 							price={tier.price}
 							list={tier.list}
 							best={tier.best}
+							selectTier={tierSelect}
+							isActive={activeTier}
 						/>
 					))}
+					
 				</div>
 			</div>
 		</section>
--- src/components/UI/TierCard.jsx
+++ src/components/UI/TierCard.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
-import { FaDollarSign } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
 
-const TierCard = ({ title, subtitle, price, list, best }) => {
+const TierCard = ({
+	title,
+	subtitle,
+	price,
+	list,
+	best,
+	selectTier,
+	isActive,
+}) => {
+	const activeTier = title === isActive;
+
 	return (
-		<div className="tierCard">
-			<div className="tier__header">
-				<div className="tier__title">
-					<span className=" text__color--purple">
-						{title} | ${price}
-					</span>
+		<div className={`tier__Card ${activeTier ? 'active' : ''}`}>
+			<div className="tier__left">
+				<div className="tier__header">
+					<div className="tier__title">
+						<span className=" text__color--purple">
+							{title} | ${price}
+						</span>
+						
+					</div>
+					<p className="text__color--normal">{subtitle}</p>
 				</div>
-				<p className="text__color--normal">{subtitle}</p>
+
+				<div className="tier__description">
+					<ul className=" tier__list text__color--muted">
+						{list.map((e, index) => (
+							<li className="text__color--blue" key={index}>
+								{e}
+							</li>
+						))}
+					</ul>
+					<p className="text__color--muted">{best}</p>
+				</div>
 			</div>
-			<div className="tier__description">
-				<ul className="text__color--muted">
-					{list.map((e, index) => (
-						<li className="text__color--blue" key={index}>
-							{e}
-						</li>
-					))}
-				</ul>
-				<p className="text__color--muted">{best}</p>
-			</div>
+			<button onClick={() => selectTier(title)}>
+				<FaPlus className="" />
+			</button>
 		</div>
 	);
 };
